1c57cc711d5296eb6078305f32289d1adb027454
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-11-06 03:27:48 +0100

    added update and delete functionality

--- src/pages/Note.js
+++ src/pages/Note.js
@@ -1,10 +1,11 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, Navigate } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 import { useEffect, useState } from "react";
 
 const Note = () => {
 	const { id } = useParams();
 	const [note, setNote] = useState(null);
+	const [isRedirecting, setRedirecting] = useState(false);
 
 	useEffect(() => {
 		const getNote = async () => {
@@ -15,17 +16,50 @@ const Note = () => {
 		getNote();
 	}, [id]);
 
+	const updateNote = async () => {
+		await fetch(`http://localhost:5000/notes/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ ...note, updated: new Date() }),
+		});
+	};
+
+	const deleteNote = async () => {
+		await fetch(`http://localhost:5000/notes/${id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-type": "application/json",
+			},
+		});
+		setRedirecting(true);
+	};
+
+	const handleSubmit = () => {
+		updateNote();
+	};
+
+	if (isRedirecting) {
+		return <Navigate to={"/"} />;
+	}
+
 	return (
 		<div className="note">
 			<div className="note-header">
 				<h3>
 					<Link to={"/"}>
-						<ArrowLeft />
+						<ArrowLeft onClick={handleSubmit} />
 					</Link>
 				</h3>
+				<button onClick={deleteNote}>Delete</button>
 			</div>
 
-			<textarea value={note?.body}></textarea>
+			<textarea
+				onChange={(e) => {
+					setNote({ ...note, body: e.target.value });
+				}}
+				value={note?.body}></textarea>
 		</div>
 	);
 };
