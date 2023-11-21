f0fbe90ba22ece2f751637869394799d120e835a
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-11-22 00:59:40 +0100

    feature added light/dark mode toggle btn

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Header from "./components/Header";
 import NotesPage from "./pages/NotesPage";
 import Note from "./pages/Note";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
+	function handleColorModeToggle() {
+		setIsDarkMode((prevMode) => !prevMode);
+	}
+
 	return (
 		<Router>
-			<div className="container dark">
+			<div className={`container ${isDarkMode ? "dark" : ""}`}>
 				<div className="app">
-					<Header />
+					<Header onColorModeToggle={handleColorModeToggle} />
 					<Routes>
 						<Route path="/" element={<NotesPage />} />
 						<Route path="/note/:id" element={<Note />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-const Header = () => {
+import colorModeImg from '../assets/brightness-half.svg'
+const Header = ({onColorModeToggle}) => {
 	return (
 		<div className="app-header">
 			<h1>Your Notes</h1>
+			<button onClick={onColorModeToggle}><img src={colorModeImg} alt="change color mode icon" /></button>
 		</div>
 	);
 };
